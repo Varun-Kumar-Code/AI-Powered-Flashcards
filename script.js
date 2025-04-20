dde7654c5c14da3f78bfdb217f074a85fc55f707
Varun Kumar <varunkumar1329@gmail.com>
2025-04-20 10:05:27 +0530

    commit it

--- script.js
+++ script.js
@@ -379,23 +379,3 @@ document.getElementById('signupForm').addEventListener('submit', async (e) => {
         alert('An error occurred. Please try again.');
     }
 });
-
-//Prevent righ click
-document.oncontextmenu = () => {
-    alert("Don't try right click")
-    return false
-}
-
-document.onkeydown = e => {
-    //Prevent F12 key
-    if (e.key == "F12") {
-        alert("Don't try to inspect element")
-        return false
-    }
-
-    //Prevent showing page source by ctrl + U
-    if (e.ctrlKey && e.key == "u") {
-        alert("Don't try to view page sources")
-        return false
-    }
-}
